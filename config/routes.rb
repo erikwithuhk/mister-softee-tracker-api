e9a2c32d59d3974a0c59132422a8cee7bab0742c
Erik Jönsson <efjonsson@gmail.com>
2016-10-30 14:45:09 -0400

    namespace api through subdomain, ensure reqests are json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api,
+            defaults: { format: :json },
+            constraints: { subdomain: 'api' },
+            path: '/' do
     scope module: :v1 do
       resources :users
     end
