33d3c20540eca23e7ecba84163cf710c8839e2c8
Erik Jönsson <efjonsson@gmail.com>
2016-11-01 13:05:34 -0400

    authenticate user after creating

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,8 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::UsersController < Api::V1::AuthController
   skip_before_action :authenticateRequest!, only: [:create]
   before_action :find_user, only: [:show, :update, :destroy]
 
   def index
-    logger.info request.env["Headers"]
     users = User.all
     render json: users, status: :ok
   end
@@ -15,8 +14,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      # session[:user_id] = user.id
-      render json: user, status: :ok
+      authenticate
     else
       render json: { errors: user.errors },
              status: :unprocessable_entity
