3faacdfb5fcc623c6d50d982c21a28a3f51058b7
Erik Jönsson <efjonsson@gmail.com>
2016-11-01 13:08:30 -0400

    change login path

--- app/controllers/api/v1/--sessions_controller.rb
+++ app/controllers/api/v1/--sessions_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      # session[:user_id] = user.id
       render json: session, status: :ok
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'signup'  => 'users#create'
-      post 'login'  => 'sessions#create'
-      delete 'logout' => 'sessions#destroy'
       resources :users
-      post "authenticate" => "auth#authenticate"
+      post "login" => "auth#authenticate"
     end
   end
 end
