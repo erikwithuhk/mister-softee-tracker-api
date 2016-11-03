5e6602855b4c7b62a18ddfe4b6b21149654a18a1
Erik Jönsson <efjonsson@gmail.com>
2016-11-03 16:46:30 -0400

    add cors headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ end
 class ApplicationController < ActionController::API
   attr_reader :current_user
 
+  protect_from_forgery with: :exception
+
   before_action :authenticateRequest!
 
   respond_to :json
@@ -56,5 +58,28 @@ class ApplicationController < ActionController::API
   def user_not_authenticated
     render json: { errors: ['Not Authenticated'] },
            status: :unauthorized
+  end
+
+  # http://www.tsheffler.com/blog/2011/02/22/cross-origin-resource-sharing-for-json-and-rails/
+  # For all responses in this controller, return the CORS access control headers.
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
   end
 end
