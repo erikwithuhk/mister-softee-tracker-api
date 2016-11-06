b5ce93721991b3fedc17020e9a5179db25fa5c35
Erik Jönsson <efjonsson@gmail.com>
2016-11-06 15:27:08 -0500

    add request validations

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,8 @@
 class Request < ApplicationRecord
+  validates_presence_of :customer_id
+  validates_presence_of :vendor_id
+  validates_presence_of :status
+  validates_inclusion_of :status, in: %w( pending approved rejected ),
+                                  message: "{{value}} is not a valid status"
+  validates_presence_of :isExpired
 end
